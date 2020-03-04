76c98ee3e0cc4377cb93b9db335e458e98407085
Christopher <Christo@ChristophersMBP.int.crosemont.qc.ca>
2020-03-04 10:37:12 -0500

    Ajout initial

---
+++ ProjetTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProjetTest
+//
+//  Created by Christopher Lajoie on 2020-03-04.
+//  Copyright © 2020 Christopher Lajoie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
